f9af06c1d31f552a44fbbfed840f40d6845aedad
Robert Shiue <robertdx@gmail.com>
2024-02-28 18:43:42 +0800

    impl RAG pipeline

--- demo.py
+++ demo.py
@@ -3,6 +3,7 @@ import os
 from langchain_community.document_loaders import UnstructuredHTMLLoader
 from langchain_community.document_loaders import BSHTMLLoader
 from langchain_community.document_loaders import UnstructuredMarkdownLoader
+from langchain.text_splitter import CharacterTextSplitter
 from operator import itemgetter
 
 from langchain_community.vectorstores import FAISS
@@ -13,12 +14,14 @@ from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from os import environ
 from bs4 import BeautifulSoup
 
+from rich.console import Console
+
 import logging
 
 OPENAI_API_KEY = environ.get("OPENAI_API_KEY")
 GITHUB_URL_LANGCHAIN = r"https://github.com/langchain-ai/langchain/releases/tag/"
 
-logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+logging.basicConfig(level=logging.INFO, format="%(asctime)s %(name)s [%(levelname)s] %(message)s")
 logger = logging.getLogger("LangChain Demo")
 
 def get_changelogs():
@@ -27,8 +30,8 @@ def get_changelogs():
     with open("tags.md", "r") as f:
         lines = f.readlines()
         import math
-        # LIMIT = math.inf
-        LIMIT = 5
+        LIMIT = math.inf
+        LIMIT = 100
         count = 0
         for line in lines:
             if count >= LIMIT:
@@ -37,7 +40,7 @@ def get_changelogs():
             tag = line.strip()
             url_tags.append(f"{GITHUB_URL_LANGCHAIN}" + tag)
 
-    logger.info(url_tags)
+    logger.info("\n".join(url_tags))
     import requests
     collection = []
     # make dir at `./tag_pages/`
@@ -49,87 +52,109 @@ def get_changelogs():
         # first, check `./tag_pages/` to see if the page has already been downloaded.
         # if it has, load the file 
         # if it hasn't, continue to load the web page
-
-        if url.split('/')[-1] in os.listdir("tag_pages"):
-
-            with open(f"tag_pages/{url.split('/')[-1]}.html", "r") as f:
-                content = f.read()
-                collection.append(content)
-                continue
-
-        # use `request` to GET the remote web page.
-        # fire GET 
-        logger.info(f"loading content from {url}")
-        response = requests.get(url)
         
-        # convert html to plain text, using `BeautifulSoup`
-        soup = BeautifulSoup(response.text, "html.parser")
+        tagname = url.split('/')[-1]
+        if f"{tagname}.html" in os.listdir("tag_pages"):
 
-        # write the HTML content to a file at `./tag_pages/`
-        with open(f"tag_pages/{url.split('/')[-1]}.html", "w") as f:
-            f.write(soup.prettify())
+            
+            with open(f"tag_pages/{tagname}.html", "r") as f:
+                content = f.read()
+                # do data cleansing...
+                content = BeautifulSoup(content, "html.parser").get_text()
+                # remove leading and trailing whitespaces
+                content = content.strip()
+                # remove duplicated line breaks: `\n`
+                content = "\n".join([line for line in content.split("\n") if line.strip() != ""])
+                logger.info(content[:100])
+                collection.append(content)
+            
+            logger.info(f"[cached]loading content from {url}")
+            
+        else: 
+            
+            logger.info(f"loading content from remote: {url}")
+            response = requests.get(url)
+            
+            # convert html to plain text, using `BeautifulSoup`
+            soup = BeautifulSoup(response.text, "html.parser")
 
-        # loader = BSHTMLLoader(soup.get_text())
-        # loader = UnstructuredMarkdownLoader(soup.get_text())
-        # loader.load()
-        collection.append(soup.get_text())
+            # write the HTML content to a file at `./tag_pages/`
+            with open(f"tag_pages/{tagname}.html", "w") as f:
+                f.write(soup.prettify())
+
+            plain_text = soup.get_text()
+            logger.info(f"plain_text: {plain_text}")
+            collection.append(plain_text)
 
     return collection
     
-
-
-def retrieve():
-    loader = UnstructuredHTMLLoader()
-
-def get_promps():
-    pass
-
-def llm():
-    pass
-
-
-def temp():
+def main():
 
     content = get_changelogs()
-
-    logger.info(f"content: {content}")
-
-    vectorstore = FAISS.from_texts(
-        content, embedding=OpenAIEmbeddings()
-    )
-    retriever = vectorstore.as_retriever()
-
-    template = """Answer the question based only on the following context:
-    {context}
-
-    Question: {question}
-    """
-    prompt = ChatPromptTemplate.from_template(template)
-
-    model = ChatOpenAI(api_key=OPENAI_API_KEY, model="gpt-4-turbo-preview")
-
-    chain = ( 
-            { "context": retriever, "question": RunnablePassthrough()}
-            | prompt 
-            | model
-            | StrOutputParser()
-             
-             )
     
-    QUESTION = """what is the major changes between theses versions?
-"""
-    result = chain.invoke(QUESTION)
-    logger.info(result)
+    logger.debug(f"content: {content}")
+
+    logger.info("preparing vectorstore ... ")
+    if False and "faiss.index" in os.listdir():
+        from langchain.vectorstores.utils import DistanceStrategy
+        logger.info(f"Found existed vectorstore.")
+        vectorstore = FAISS.load_local( "./faiss.index", OpenAIEmbeddings(), distance_strategy=DistanceStrategy.COSINE)
+    else:
+
+        embedding = OpenAIEmbeddings()
+        
+        text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=32)
+        docs = []
+        for item in content:
+            docs.extend(text_splitter.split_text(item))
+        # docs = [ text_splitter.split_text(item) for item in content ]
+        vectorstore = FAISS.from_texts(
+            docs, embedding=embedding
+        )
+        vectorstore.save_local("faiss.index")
+
+    QUESTION = """what is the major system design changes in these versions?"""
+
+    # retreived_docs = vectorstore.similarity_search(QUESTION)
+
+    # logger.info(f"retreived_docs: {retreived_docs[0].page_content}")
+
+    retriever = vectorstore.as_retriever()
+    # r_docs = retriever.invoke(QUESTION)
+    # logger.info(f"r_docs: {r_docs[0].page_content}")
+
+    from langchain import hub
+
+    prompt = hub.pull("rlm/rag-prompt")
 
 
-
-def main():
-    logger.info(f"default key {OPENAI_API_KEY}")
-    # urls = get_changelogs()
-    # logger.info(f"found tags:\n{urls}")
-    temp()
+    llm = ChatOpenAI(api_key=OPENAI_API_KEY, model="gpt-4-turbo-preview")
 
 
+    def format_docs(docs):
+        logger.info(f"docs to format : {len(docs)}")
+        return "\n\n".join(doc.page_content for doc in docs)
+
+
+    rag_chain = (
+        {"context": retriever | format_docs, "question": RunnablePassthrough()}
+        | prompt
+        | llm
+        | StrOutputParser()
+    )
+
+    result = rag_chain.invoke(QUESTION)
+
+
+    print(result)
+
+    
+
+    
 
 if __name__ == "__main__":
+    
+    assert OPENAI_API_KEY is not None, "OpenAI API key is not set"
+
+    
     main()
---
+++ my-note.md
@@ -0,0 +1,9 @@
+Always use `set=PYTHONIOENCODING=utf8` before calling any scripts of poetry.
+
+Run make test as follows in Windows, in libs/community:
+```bash
+set PYTHONIOENCODING=utf8
+"c:\Program Files (x86)\GnuWin32\bin\make" test
+```
+
+
